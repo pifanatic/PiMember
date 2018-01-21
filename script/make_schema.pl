7b667637544425e063fd2bfacaa793ba4e9232ff
Kai Mörker <kai.moerker@gmail.com>
2018-01-21 20:36:21 +0100

    add script to generate Schemas
    This script will take the name of a SQLite database as the first
    and only argument. It then generates the Schemass for all tables
    that the database contains in 'PiMember/'.

---
+++ script/make_schema.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+use DBIx::Class::Schema::Loader qw/ make_schema_at /;
+
+if ($#ARGV < 0) {
+    print "Please provide a database name!\n";
+    exit 1;
+}
+
+my $dbname = $ARGV[0];
+
+make_schema_at(
+    'PiMember::Schema',
+    {
+        debug => 1,
+        dump_directory => './',
+    },
+    [
+        "dbi:SQLite:$dbname"
+    ],
+);
