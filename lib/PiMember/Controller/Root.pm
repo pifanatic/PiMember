0a055432b51ea32af533c3188075b854a99210bf
Kai Mörker <kai.moerker@gmail.com>
2020-03-29 14:18:26 +0200

    remove superfluous comments

--- lib/PiMember/Controller/Root.pm
+++ lib/PiMember/Controller/Root.pm
@@ -4,37 +4,9 @@ use namespace::autoclean;
 
 BEGIN { extends 'Catalyst::Controller' }
 
-#
-# Sets the actions in this controller to be registered with no prefix
-# so they function identically to actions created in MyApp.pm
-#
 __PACKAGE__->config(namespace => '');
 
-=encoding utf-8
-
-=head1 NAME
-
-PiMember::Controller::Root - Root Controller for PiMember
-
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 METHODS
-
-=head2 index
-
-The root page (/)
-
-=cut
-
 sub index :Path :Args(0) {}
-
-=head2 default
-
-Standard 404 error page
-
-=cut
 
 sub default :Path {
     my ( $self, $c ) = @_;
@@ -42,12 +14,6 @@ sub default :Path {
     $c->response->body( 'Page not found' );
     $c->response->status(404);
 }
-
-=head2 end
-
-Attempt to render a view, if needed.
-
-=cut
 
 sub end : ActionClass('RenderView') {}
 
