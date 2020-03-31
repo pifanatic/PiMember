3b59d917ad298a2d8a1a87232ff17a977f1c1902
Kai Mörker <kai.moerker@gmail.com>
2020-03-31 20:05:19 +0200

    implement basic login logic

--- lib/PiMember/Controller/Login.pm
+++ lib/PiMember/Controller/Login.pm
@@ -4,7 +4,24 @@ use namespace::autoclean;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
-sub index : Path Args(0) { }
+sub index : Path Args(0) {
+    my ($self, $c) = @_;
+
+    if ($c->req->method eq 'POST') {
+        my $username = $c->req->params->{"username"};
+        my $password = $c->req->params->{"password"};
+
+        if ($username && $password) {
+            if ($c->authenticate({ username => $username, password => $password })) {
+                $c->response->redirect($c->uri_for("/"));
+            } else {
+                $c->stash({ error_msg => "Incorrect username or password." });
+            }
+        } else {
+            $c->stash({ error_msg => "Username and password required." });
+        }
+    }
+}
 
 __PACKAGE__->meta->make_immutable;
 
