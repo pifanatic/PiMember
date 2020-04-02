3912f8afbc8c43993fa8270cd329bc908643aacc
Kai Mörker <kai.moerker@gmail.com>
2020-04-02 11:55:54 +0200

    implement basic card creation logic

--- lib/PiMember/Controller/Cards.pm
+++ lib/PiMember/Controller/Cards.pm
@@ -2,10 +2,30 @@ package PiMember::Controller::Cards;
 use Moose;
 use namespace::autoclean;
 
+use DateTime;
+
 BEGIN { extends 'Catalyst::Controller'; }
 
 sub add : Local Args(0) {
-    
+    my ($self, $c) = @_;
+
+    if ($c->req->method eq "POST") {
+        my $title      = $c->req->params->{title};
+        my $front_text = $c->req->params->{frontText};
+        my $back_text  = $c->req->params->{backText};
+        my $now        = DateTime->now->dmy("/");
+
+        $c->model("DB::Card")->create({
+            title              => $title,
+            frontside          => $front_text,
+            backside           => $back_text,
+            rating             => 0,
+            last_seen          => $now,
+            due                => $now,
+            correctly_answered => 0,
+            wrongly_answered   => 0,
+        });
+    }
 }
 
 __PACKAGE__->meta->make_immutable;
