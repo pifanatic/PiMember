458ee5edc926f17a0480498d9163eb0224a1f163
Kai Mörker <kai.moerker@gmail.com>
2020-04-03 11:12:15 +0200

    make sample data parsable
    DateTime::Format::SQLite prefers the YYYY-MM-DD format over the
    DD/MM/YYYY format

--- data/samples.sql
+++ data/samples.sql
@@ -13,15 +13,15 @@ VALUES
 INSERT INTO 'Cards'
 (title, frontside, backside, category_id, rating, last_seen, due, correctly_answered, wrongly_answered)
 VALUES
-('Capital of Germany', 'What''s the capital of Germany?', 'Berlin', 1, 1, '01/01/2018', '02/01/2018', 1, 2),
-('Capital of Spain', 'What''s the capital of Spain?', 'Madrid', 1, 2, '01/01/2018', '03/01/2018', 3, 2),
-('Capital of France', 'What''s the capital of France?', 'Paris', 1, 1, '01/01/2018', '02/01/2018', 4, 2),
-('Capital of the United States', 'What''s the capital of the United States?', 'Washington D.C.', 1, 3, '01/01/2018', '04/01/2018', 6, 0),
-('German World Cup triumphs', 'How many World Cups has Germany won?', '4', 2, 1, '01/01/2018', '02/01/2018', 1, 2),
-('Spanish World Cup triumphs', 'How many World Cups has Spain won?', '1', 2, 2, '01/01/2018', '03/01/2018', 4, 2),
-('French World Cup triumphs', 'How many World Cups has France won?', '1', 3, 1, '01/01/2018', '04/01/2018', 5, 0),
-('U.S. World Cup triumphs', 'How many World Cups have the U.S. won?', '0', 2, 1, '01/01/2018', '02/01/2018', 1, 2),
-('Inhabitans of Germany', 'How many inhabitants does Germany have?', '82.5 Millions', 3, 1, '01/01/2018', '02/01/2018', 1, 2),
-('Inhabitans of Spain', 'How many inhabitants does Spain have?', '46.4 Millions', 3, 2, '01/01/2018', '03/01/2018', 1, 2),
-('Inhabitans of France', 'How many inhabitants does France have?', '66.9 Millions', 3, 1, '01/01/2018', '02/01/2018', 1, 2),
-('Inhabitans of the U.S.', 'How many inhabitants do the U.S. have?', '322.7 Millions', 3, 4, '01/01/2018', '06/01/2018', 7, 2);
+('Capital of Germany', 'What''s the capital of Germany?', 'Berlin', 1, 1, '2018-01-01', '2018-01-02', 1, 2),
+('Capital of Spain', 'What''s the capital of Spain?', 'Madrid', 1, 2, '2018-01-01', '2018-01-03', 3, 2),
+('Capital of France', 'What''s the capital of France?', 'Paris', 1, 1, '2018-01-01', '2018-01-02', 4, 2),
+('Capital of the United States', 'What''s the capital of the United States?', 'Washington D.C.', 1, 3, '2018-01-01', '2018-01-04', 6, 0),
+('German World Cup triumphs', 'How many World Cups has Germany won?', '4', 2, 1, '2018-01-01', '2018-01-02', 1, 2),
+('Spanish World Cup triumphs', 'How many World Cups has Spain won?', '1', 2, 2, '2018-01-01', '2018-01-03', 4, 2),
+('French World Cup triumphs', 'How many World Cups has France won?', '1', 3, 1, '2018-01-01', '2018-01-04', 5, 0),
+('U.S. World Cup triumphs', 'How many World Cups have the U.S. won?', '0', 2, 1, '2018-01-01', '2018-01-02', 1, 2),
+('Inhabitans of Germany', 'How many inhabitants does Germany have?', '82.5 Millions', 3, 1, '2018-01-01', '2018-01-02', 1, 2),
+('Inhabitans of Spain', 'How many inhabitants does Spain have?', '46.4 Millions', 3, 2, '2018-01-01', '2018-01-03', 1, 2),
+('Inhabitans of France', 'How many inhabitants does France have?', '66.9 Millions', 3, 1, '2018-01-01', '2018-01-02', 1, 2),
+('Inhabitans of the U.S.', 'How many inhabitants do the U.S. have?', '322.7 Millions', 3, 4, '2018-01-01', '2018-01-06', 7, 2);
