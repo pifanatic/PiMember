04cd00dc4398fe3c34960b667fa935657274e137
Kai Mörker <kai.moerker@gmail.com>
2020-04-21 21:25:23 +0200

    PiMember::ActionRole::UpdateQueue - add pod

--- lib/PiMember/ActionRole/UpdateQueue.pm
+++ lib/PiMember/ActionRole/UpdateQueue.pm
@@ -2,11 +2,23 @@ package PiMember::ActionRole::UpdateQueue;
 
 use Moose::Role;
 
+=head1 NAME
+
+PiMember::ActionRole::UpdateQueue;
+
+=head1 DESCRIPTION
+
+ActionRole that updates the queue of all cards that need to be learned by
+querying the database.
+
+Update will only be made after a POST request since this is the only request
+type that can actually have an impact on the cards that are due.
+
+=cut
+
 after "execute" => sub {
     my ($self, $controller, $c) = @_;
 
-    # Only retrieve list of due cards from database after a change has been made
-    # Such a change can only occur after a POST request
     if ($c->req->method eq "POST") {
         my @cards_to_learn = $c->model("DB::Card")->search(
             { due      => { "<=" => DateTime->now->iso8601 } },
@@ -18,3 +30,16 @@ after "execute" => sub {
 };
 
 1;
+
+=encoding utf8
+
+=head1 AUTHOR
+
+Kai Mörker
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
