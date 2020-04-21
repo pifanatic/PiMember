db83aaa0ea745eada52fd6e60761b36ebce97109
Kai Mörker <kai.moerker@gmail.com>
2020-04-21 21:26:43 +0200

    PiMember::View::HTML - fix formatting

--- lib/PiMember/View/HTML.pm
+++ lib/PiMember/View/HTML.pm
@@ -6,8 +6,8 @@ extends "Catalyst::View::TT";
 
 __PACKAGE__->config(
     TEMPLATE_EXTENSION => ".tt",
-    render_die => 1,
-    WRAPPER => "wrapper.tt"
+    render_die         => 1,
+    WRAPPER            => "wrapper.tt"
 );
 
 1;
