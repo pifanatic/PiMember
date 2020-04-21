09ec456d0493fbe1f5f8bd5c8ce2a3961b15d73b
Kai Mörker <kai.moerker@gmail.com>
2020-04-21 21:19:03 +0200

    Controller::Logout - add pod

--- lib/PiMember/Controller/Logout.pm
+++ lib/PiMember/Controller/Logout.pm
@@ -2,7 +2,25 @@ package PiMember::Controller::Logout;
 use Moose;
 use namespace::autoclean;
 
+=head1 NAME
+
+PiMember::Controller::Logout - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Handles the logout process
+
+=cut
+
 BEGIN { extends "Catalyst::Controller"; }
+
+=head1 METHODS
+
+=head2 index
+
+The logout routine
+
+=cut
 
 sub index : Path Args(0) {
     my ($self, $c) = @_;
@@ -16,3 +34,16 @@ sub index : Path Args(0) {
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+=encoding utf8
+
+=head1 AUTHOR
+
+Kai Mörker
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
