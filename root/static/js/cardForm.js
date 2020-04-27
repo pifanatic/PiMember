dec1d0e9ee840330b7df4e7c68f23a38228cf7f3
Kai Mörker <kai.moerker@gmail.com>
2020-04-27 17:15:42 +0200

    add_edit.tt - extract JavaScript code

---
+++ root/static/js/cardForm.js
@@ -0,0 +1,28 @@
+(function() {
+    function setPreviewValue(previewEl, value) {
+        previewEl.innerHTML = value;
+    }
+
+    let frontInput = document.getElementById("front-input"),
+        frontPreview = document.getElementById("front-preview"),
+        backInput = document.getElementById("back-input"),
+        backPreview = document.getElementById("back-preview"),
+        form = document.getElementById("cardForm"),
+        inputPreviewMap = {
+            "front-input": frontPreview,
+            "back-input": backPreview
+        };
+
+    form.addEventListener("keyup", e => {
+        let previewEl = inputPreviewMap[e.target.id];
+
+        if (!previewEl) {
+            return;
+        }
+
+        setPreviewValue(previewEl, e.target.value);
+    });
+
+    setPreviewValue(frontPreview, frontInput.value);
+    setPreviewValue(backPreview, backInput.value);
+})();
