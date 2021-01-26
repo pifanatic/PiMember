e60a937e14db6389933c2a6becb4159df09609dc
Kai Mörker <kai.moerker@gmail.com>
2021-01-26 11:06:53 +0100

    Fix mis-aligned text in <pre> elements
    We know retrieve the text that PIML has to parse with the textContent
    attribute. Unlike innerText, textContent preserves whitespaces which
    can therefore be displayed with <pre> elements.

--- root/static/js/learn.js
+++ root/static/js/learn.js
@@ -30,5 +30,5 @@ document.body.addEventListener("keyup", e => {
 let front = document.getElementById("front-text"),
     back  = document.getElementById("back-text");
 
-front.innerHTML = PIML.parse(front.innerText.trim());
-back.innerHTML  = PIML.parse(back.innerText.trim());
+front.innerHTML = PIML.parse(front.textContent.trim());
+back.innerHTML  = PIML.parse(back.textContent.trim());
--- root/static/js/view.js
+++ root/static/js/view.js
@@ -3,5 +3,5 @@ import PIML from "./piml.min.js";
 let front = document.getElementById("front-text"),
     back  = document.getElementById("back-text");
 
-front.innerHTML = PIML.parse(front.innerText.trim());
-back.innerHTML  = PIML.parse(back.innerText.trim());
+front.innerHTML = PIML.parse(front.textContent.trim());
+back.innerHTML  = PIML.parse(back.textContent.trim());
