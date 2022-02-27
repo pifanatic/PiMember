c00cc9344693e66b6464d9262c81c83ac22c2a7b
Kai Mörker <kai.moerker@gmail.com>
2022-02-27 21:55:48 +0100

    make_schema - do not load external files

--- script/make_schema.sh
+++ script/make_schema.sh
@@ -6,5 +6,6 @@ script/pimember_create.pl  \
     create=static          \
     use_moose=1            \
     overwrite_modifications=1 \
+    skip_load_external=1   \
     dbi:SQLite:pimember.db \
     on_connect_do="PRAGMA foreign_keys = ON"
